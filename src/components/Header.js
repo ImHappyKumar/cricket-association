73cf75c94c2d7b1c3ee7a4bd7cd882cbceff7ef4
ImHappyKumar <hkh7262@gmail.com>
2023-10-15 17:57:49 +0530

    3

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,21 +6,27 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top py-0">
       <div className="container">
-        <div className="d-flex justify-content-between align-items-center w-100">
-          <NavLink className="navbar-brand" to="/">
-            <img src={Logo} alt="logo" className="w-75" />
-          </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+        <div className="d-lg-flex justify-content-between align-items-center w-100">
+          <div className="d-flex justify-content-between align-items-center">
+            <NavLink className="navbar-brand" to="/">
+              <img
+                src={Logo}
+                alt="logo"
+                style={{ width: "55px" }}
+              />
+            </NavLink>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item me-4">
